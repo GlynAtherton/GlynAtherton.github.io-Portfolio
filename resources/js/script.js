086ffdf0e6500b7a42fd36853d00d761f17b17e9
GlynAtherton <gatherto@yahoo.co.uk>
2021-06-17 19:09:27 +0100

    First commit

---
+++ resources/js/script.js
@@ -0,0 +1,9 @@
+function imageEnlarge(imgID) {
+    //document.getElementById(imgID).style.height = "40%";
+    document.getElementById(imgID).style.width = "50%";
+ }
+
+function imageReset(imgID) {
+    //document.getElementById(imgID).style.height = "20%";
+    document.getElementById(imgID).style.width = "30%";
+ }
